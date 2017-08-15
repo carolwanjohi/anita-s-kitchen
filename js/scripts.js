e886eb69e6f95925d4ee9161c326d8a26d2e05ea
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-08-15 22:52:21 +0300

    Add javascript to section 6 Add alert message to thank the user for subscribing in section 6.

--- js/scripts.js
+++ js/scripts.js
@@ -24,4 +24,16 @@ $(document).ready(function() {
         $(this).stop().animate({ opacity: 1.0 }, "fast").css("color", "white"); //on mouse out
     // });
     });
+
+    // Subscription message
+    $("form#subscribeForm").submit(function(event) {
+    var userEmailInput = $("input#subscribeEmail").val();
+
+    alert("userEmailInput" + " thank you for subscribing.");
+
+    // $(".userEmail").text(userEmailInput);
+
+    // $("#response").show();
+    event.preventDefault();
+  });
 });
